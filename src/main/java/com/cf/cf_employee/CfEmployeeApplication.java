b9d767032bc38f49e22ac26ca6ff7a60e03ca3ea
sweisser <weisser.sarah114@gmail.com>
2023-11-16 20:43:43 -0500

    initial commit of spring project

---
+++ src/main/java/com/cf/cf_employee/CfEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.cf.cf_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cf/cf_employee/CfEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.cf_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
